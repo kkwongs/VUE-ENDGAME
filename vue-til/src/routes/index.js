821b5844b8a34885b8869bf4af1e1824449d4dc4
kkwongs <kihyun5741@gmail.com>
2020-04-24 16:08:45 +0900

    페이지 컴포넌트 생성 및 연결

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import LoginPage from '@/views/LoginPage.vue';
+import SignupPage from '@/views/SignupPage.vue';
 
 // 플러그인을 초기화하기 위한 코드
 Vue.use(VueRouter);
 
 // 인스턴스 생성후 파일에서 밖으로 내보냄
-export default new VueRouter();
+export default new VueRouter({
+  routes: [
+    {
+      path: '/login',
+      component: LoginPage,
+    },
+    {
+      path: '/signup',
+      component: SignupPage,
+    },
+  ],
+});
