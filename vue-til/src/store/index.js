6a1805aed23d65c433cdeb3b12bf72f2a3628872
kkwongs <kihyun5741@gmail.com>
2020-04-26 17:25:56 +0900

    Header에 ID(username) 표시

--- vue-til/src/store/index.js
+++ vue-til/src/store/index.js
@@ -4,7 +4,14 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // 여러 컴포넌트 간의 공유되는 데이터
   state: {
     username: '',
+  },
+  // 데이터를 바꿈
+  mutations: {
+    setUsername(state, username) {
+      state.username = username;
+    },
   },
 });
