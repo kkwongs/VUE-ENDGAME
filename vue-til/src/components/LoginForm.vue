1631d35e45090a83deab82c57a860aeb7474d3dd
kkwongs <kihyun5741@gmail.com>
2020-04-26 14:46:02 +0900

    LoginForm Markup

---
+++ vue-til/src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">ID: </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="passowrd">PW: </label>
+      <input id="password" type="text" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
