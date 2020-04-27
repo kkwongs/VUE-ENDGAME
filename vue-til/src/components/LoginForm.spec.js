d2209574bde699777d4501a277e46804e591e014
kkwongs <kihyun5741@gmail.com>
2020-04-27 23:52:01 +0900

    @vue/test-utils 소개 및 적용

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -1,9 +1,12 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+// import Vue from 'vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    const instance = new Vue(LoginForm).$mount();
-    expect(instance.username).toBe('');
+    // const instance = new Vue(LoginForm).$mount();
+    const wrapper = shallowMount(LoginForm);
+    // wrapper.vm.username
+    expect(wrapper.vm.username).toBe('');
   });
 });
