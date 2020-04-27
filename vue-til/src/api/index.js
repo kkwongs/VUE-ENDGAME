d63cbaf758971456e2fb49a64c29d874b7fc4c1f
kkwongs <kihyun5741@gmail.com>
2020-04-27 20:57:41 +0900

    API 함수 모듈화

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -8,39 +8,17 @@ function createInstance() {
 }
 
 // 액시오스 초기화 함수
-function createInstanceWithAuth() {
+function createInstanceWithAuth(url) {
   const instance = axios.create({
     baseURL: `${process.env.VUE_APP_API_URL}${url}`,
   });
   return setInterceptors(instance);
 }
 
-const instance = createInstance();
-const posts = createInstanceWithAuth('posts');
-
-// 회원가입 API
-function registerUser(userData) {
-  return instance.post('signup', userData);
-}
-
-// 로그인 API
-function loginUser(userData) {
-  return instance.post('login', userData);
-}
-
-// 학습 노트 데이터를 조회하는 API
-function fetchPosts() {
-  return instance.get('posts');
-}
-
-// 학습 노트 데이터를 생성하는 API
-function createPost(postData) {
-  return instance.post('posts', postData);
-}
+export const instance = createInstance();
+export const posts = createInstanceWithAuth('posts');
 
 // GET - posts
 // POST - post
 // PUT - posts {id}
 // DELETE - posts {id}
-
-export { registerUser, loginUser, fetchPosts, createPost };
