0ebb3b21df628a95fd61a6927248707e4a418f39
kkwongs <kihyun5741@gmail.com>
2020-04-27 19:00:12 +0900

    actions속성을 이용한 로그인 기능 구현

--- vue-til/src/store/index.js
+++ vue-til/src/store/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { getAuthFromCookie, getUserFromCookie } from '@/utils/cookies';
+import {
+  getAuthFromCookie,
+  getUserFromCookie,
+  saveAuthToCookie,
+  saveUserToCookie,
+} from '@/utils/cookies';
+import { loginUser } from '@/api/index';
 
 Vue.use(Vuex);
 
@@ -26,6 +32,17 @@ export default new Vuex.Store({
     },
     setToken(state, token) {
       state.token = token;
+    },
+  },
+  actions: {
+    async LOGIN({ commit }, userData) {
+      const { data } = await loginUser(userData);
+      console.log(data.token);
+      commit('setToken', data.token);
+      commit('setUsername', data.user.nickname);
+      saveAuthToCookie(data.token);
+      saveUserToCookie(data.user.username);
+      return data;
     },
   },
 });
