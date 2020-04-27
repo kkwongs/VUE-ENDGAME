8de0f117b5dc6bc841eb63de5287e513044e4723
kkwongs <kihyun5741@gmail.com>
2020-04-27 23:55:19 +0900

    find()를 이용한 컴포넌트 HTML 요소 검색

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-// import Vue from 'vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    // const instance = new Vue(LoginForm).$mount();
+  test('ID는 이메일 형식이어야 한다', () => {
     const wrapper = shallowMount(LoginForm);
-    // wrapper.vm.username
-    expect(wrapper.vm.username).toBe('');
+    const idInput = wrapper.find('#username');
+    console.log(idInput.html());
   });
 });
