69a2454ec49098c1626e779e6dfd7b1afadf26fd
kkwongs <kihyun5741@gmail.com>
2020-04-27 23:49:06 +0900

    vue component test

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -1,10 +1,9 @@
-import { sum } from '@/components/math';
+import Vue from 'vue';
+import LoginForm from '@/components/LoginForm.vue';
 
-// sum(10, 20); // 30
-
-describe('math.js', () => {
-  test('10 + 20 = 30', () => {
-    const result = sum(10, 20);
-    expect(result).not.toBe(30);
+describe('LoginForm.vue', () => {
+  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
+    const instance = new Vue(LoginForm).$mount();
+    expect(instance.username).toBe('');
   });
 });
