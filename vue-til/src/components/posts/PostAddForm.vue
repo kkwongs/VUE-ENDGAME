6862e534b388424c3b19a51a20b28a7e8ed7d7fa
kkwongs <kihyun5741@gmail.com>
2020-04-27 20:26:24 +0900

    본문 길이 유효성 검사 기능 구현

--- vue-til/src/components/posts/PostAddForm.vue
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -10,6 +10,9 @@
         <div>
           <label for="contents">Contents:</label>
           <textarea id="contents" type="text" rows="5" v-model="contents" />
+          <p v-if="!isContentsValid" class="validation-text warning">
+            Contents must be less than 200
+          </p>
         </div>
         <button type="submit" class="btn">Create</button>
       </form>
@@ -29,6 +32,11 @@ export default {
       contents: '',
       logMessage: '',
     };
+  },
+  computed: {
+    isContentsValid() {
+      return this.contents.length <= 200;
+    },
   },
   methods: {
     async submitForm() {
